41ca22ce11c71968e8f3e7a842fb82833742fc3e
leatecumsah <164488161+leatecumsah@users.noreply.github.com>
2025-01-05 16:06:00 +0100

    ff

---
+++ Media Player/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows;
+
+namespace SimpleMediaPlayer
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    
+
+    private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Pfad zur Mediendatei
+            string mediaPath = "C:\\Users\\leabu\\Downloads\\Pariah Nexus - S01E02 - Doctrine and Discipline WEBDL-1080p.mkv"; // Ändere dies auf einen Dateipfad!!!!!
+            mediaElement.Source = new Uri(mediaPath);
+            mediaElement.Play();
+        }
+
+        private void PauseButton_Click(object sender, RoutedEventArgs e)
+        {
+            mediaElement.Pause();
+        }
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            mediaElement.Stop();
+        }
+
+    } 
+}
+
