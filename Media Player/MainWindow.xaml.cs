5be52b7bcc0879e9cb32da347943e588a8d0ea64
leatecumsah <164488161+leatecumsah@users.noreply.github.com>
2025-01-22 15:35:56 +0100

    eh what
    buttons und Layout gefixed mehrmals jesses

--- Media Player/MainWindow.xaml.cs
+++ Media Player/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace SimpleMediaPlayer
 {
@@ -65,14 +66,24 @@ namespace SimpleMediaPlayer
                 Title = "Datei auswählen",
                 InitialDirectory = userMusicFolder,
                 Filter = "Videodateien|*.mp4",
-                Multiselect = true
+                Multiselect = false //true um mehrere dateien auszuwählen
             };
 
             if (openFileDialog.ShowDialog() == true)
             {
-                selectedFilePath = openFileDialog.FileName;
+                try{
+                    selectedFilePath = openFileDialog.FileName;
                 mediaElement.Source = new Uri(selectedFilePath);
-                MessageBox.Show($"Ausgewählte Datei: {selectedFilePath}");
+                //abspielen
+                mediaElement.LoadedBehavior = MediaState.Manual; // Manuelles Steuern erlauben
+                mediaElement.UnloadedBehavior = MediaState.Stop; // Stop bei Entladen?? idk how to call that ask tim
+                mediaElement.Play(); // Video starten }
+                }
+                    catch(Exception ex)
+                    {
+                    MessageBox.Show($"Fehler beim Laden der Datei: {ex.Message}", "Fehler", MessageBoxButton.OK, MessageBoxImage.Error);
+                
+                }
             }
         }
 
@@ -83,7 +94,7 @@ namespace SimpleMediaPlayer
 
         private void BTN_Maximize_Click(object sender, RoutedEventArgs e)
         {
-            if (WindowState.Maximized == WindowState)
+            if (WindowState ==WindowState.Maximized )
                 WindowState = WindowState.Normal;
             else
                 WindowState = WindowState.Maximized;
